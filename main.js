a4034c80011feac463b8789194afce5127047a14
Dmitry Konoshonkin <konoshonkin.d@gmail.com>
2024-03-14 17:51:07 +0400

    fix

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ let count = 0;
 
 document.getElementById('coin').addEventListener('click', function() {
     count++;
-    document.getElementById('counter').innerText = `Coins: ${count}`;
+    document.getElementById('counter').innerText = `Златники: ${count}`;
 
     const coin = document.getElementById('coin');
     coin.style.transform = 'scale(1.2) rotate(20deg)';
     setTimeout(() => {
         coin.style.transform = 'scale(1) rotate(0deg)';
-    }, 100);
+    }, 10);
 });
